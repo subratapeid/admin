34325ce0d7a73c64e63ae810025e3316bf6b5ee5
Subrata <subratap.eid@gmail.com>
2026-09-22 15:08:43 +0530

    changes on app

--- resources/views/layouts/basic/app.blade.php
+++ resources/views/layouts/basic/app.blade.php
@@ -13,7 +13,6 @@
             <div class="mx-auto w-full max-w-[1600px] px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
 
                 {{ $slot }}
-                <img src="{{ $assets->image('user.jpg') }}" alt="User">
             </div>
 
         </main>
